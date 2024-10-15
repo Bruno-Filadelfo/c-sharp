642232f11cc51265f652832931bdb14a06481eb5
Bruno-Filadelfo <bruno.filadelfo@outlook.com>
2024-10-14 23:55:24 -0300

    Csharp

---
+++ OperadoresAritméticos/OperadoresAritméticos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Xml;
+
+int n1 = 15 + 5 * 8;
+int n2 = (15 + 5) * 8;
+int n3 = 17 % 3;
+double n4 = 10.0 / 8.0;
+
+
+
+
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+Console.WriteLine(n4);
+
