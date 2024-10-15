7cac7ef02f14a08773dda1ada6b17fbfe75d422c
Bruno-Filadelfo <bruno.filadelfo@outlook.com>
2024-10-15 01:05:50 -0300

    Entrada de Dados

---
+++ EntradadeDados/EntradadeDados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+
+string frase = Console.ReadLine();
+Console.WriteLine("Você digitou: " + frase);
+
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
